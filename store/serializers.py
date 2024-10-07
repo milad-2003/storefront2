00996552de190c377acbbcd32692044717d79fd6
Milad Valizadeh <miladvalizadeh2003@gmail.com>
2024-10-07 21:35:49 +0330

    Using ModelSerializer instead of Serializer to avoid repetition

--- store/serializers.py
+++ store/serializers.py
@@ -3,15 +3,17 @@ from rest_framework import serializers
 from store.models import Product, Collection
 
 
-class CollectionSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
+class CollectionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Collection
+        fields = ['id', 'title']
 
 
-class ProductSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
-    price = serializers.DecimalField(max_digits=6, decimal_places=2, source='unit_price')
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'unit_price', 'price_with_tax', 'collection']
+
     price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
     collection = serializers.HyperlinkedRelatedField(
         queryset=Collection.objects.all(),
