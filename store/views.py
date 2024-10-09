fde3920c7f11d9446850aa015bcc92582a267f4a
Milad Valizadeh <miladvalizadeh2003@gmail.com>
2024-10-09 23:22:57 +0330

    Deleting objects

--- store/views.py
+++ store/views.py
@@ -20,17 +20,24 @@ def product_list(request):
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
-@api_view(['GET', 'PUT'])
+@api_view(['GET', 'PUT', 'DELETE'])
 def product_detail(request, id):
     product = get_object_or_404(Product, pk=id)
     if request.method == 'GET':
         serializer = ProductSerializer(product, context={'request': request})
         return Response(serializer.data)
+    
     elif request.method == 'PUT':
         serializer = ProductSerializer(product, data=request.data, context={'request': request})
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
+    
+    elif request.method == 'DELETE':
+        if product.orderitems.count() > 0:
+            return Response({"error": "Product cannot be deleted becuase it's associated with an orderitem."}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 @api_view()
