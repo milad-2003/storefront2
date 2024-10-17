2a6aea47a4629842720ae97da744ed3282ae0098
Milad Valizadeh <miladvalizadeh2003@gmail.com>
2024-10-17 23:59:21 +0330

    Using router for the urlpatterns

--- store/urls.py
+++ store/urls.py
@@ -1,10 +1,22 @@
 from django.urls import path
+from rest_framework.routers import DefaultRouter
 from . import views
 
+
+router = DefaultRouter()
+router.register('products', views.ProductViewSet)
+router.register('collections', views.CollectionViewSet)
+
+urlpatterns = router.urls
+
 # URLConf
-urlpatterns = [
-    path('products/', views.ProductList.as_view()),
-    path('collections/', views.CollectionList.as_view()),
-    path('products/<int:pk>', views.ProductDetail.as_view()),
-    path('collections/<int:pk>', views.CollectionDetail.as_view(), name='collection-detail'),
-]
+# We use this if we still need to include some other url pathes
+# urlpatterns = [
+#     Including the router urls at first:
+#     path('', include(router.urls))
+#     Adding other url pathes:
+#     path('products/', views.ProductList.as_view()),
+#     path('collections/', views.CollectionList.as_view()),
+#     path('products/<int:pk>', views.ProductDetail.as_view()),
+#     path('collections/<int:pk>', views.CollectionDetail.as_view(), name='collection-detail'),
+# ]
