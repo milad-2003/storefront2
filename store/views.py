31795836621901fa2cde5bcc10d5d9bc88d55fbd
Milad Valizadeh <miladvalizadeh2003@gmail.com>
2024-10-17 22:59:16 +0330

    Creating generic views

--- store/views.py
+++ store/views.py
@@ -1,6 +1,7 @@
 from django.db.models.aggregates import Count
 from django.shortcuts import get_object_or_404
 from rest_framework import status
+from rest_framework.generics import ListCreateAPIView
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -8,17 +9,28 @@ from .models import Product, Collection
 from .serializers import ProductSerializer, CollectionSerializer
 
 
-class ProductList(APIView):
-    def get(self, request):
-        queryset = Product.objects.select_related('collection').all()
-        serializer = ProductSerializer(queryset, many=True, context={'request': request})
-        return Response(serializer.data)
+class ProductList(ListCreateAPIView):
+    # ================================================================================================
+    # This method is only useful when you want to have some logic or condition for creating a queryset
+    # e.g checking for the current user and depending on its permissions, creating defferent querysets
+    # def get_queryset(self):
+    #     return Product.objects.select_related('collection').all()
+    # ================================================================================================
     
-    def post(self, request):
-        serializer = ProductSerializer(data=request.data, context={'request': request})
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    # ================================================================================================
+    # This method is only useful when you want to have some logic or condition for the serializer
+    # e.g different users or different roles can have different serializer classes
+    # def get_serializer_class(self):
+    #     return ProductSerializer
+    # ================================================================================================
+    
+    # In this case, there is no need to use the previously mentioned functions
+    # Instead we use the "queryset" and "serializer_class" attributes
+    queryset = Product.objects.select_related('collection').all()
+    serializer_class = ProductSerializer
+
+    def get_serializer_context(self):
+        return {'request': self.request}
 
 
 class ProductDetail(APIView):
@@ -42,17 +54,12 @@ class ProductDetail(APIView):
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-@api_view(['GET', 'POST'])
-def collection_list(request):
-    if request.method == 'GET':
-        queryset = Collection.objects.annotate(product_count=Count('products')).all()
-        serializer = CollectionSerializer(queryset, many=True, context={'request': request})
-        return Response(serializer.data)
-    elif request.method == 'POST':
-        serializer = CollectionSerializer(data=request.data, context={"request": request})
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+class CollectionList(ListCreateAPIView):
+    queryset = Collection.objects.annotate(product_count=Count('products')).all()
+    serializer_class = CollectionSerializer
+
+    def get_serializer_context(self):
+        return {'request': self.request}
 
 
 @api_view(['GET', 'PUT', 'DELETE'])
