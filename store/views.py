5f9962ef014a32f0330bde0a4d93bde62943b998
Milad Valizadeh <miladvalizadeh2003@gmail.com>
2024-10-27 15:05:25 +0330

    Generic filtering

--- store/views.py
+++ store/views.py
@@ -1,19 +1,18 @@
 from django.db.models.aggregates import Count
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
+from .filters import ProductFilter
 from .models import Product, Collection, OrderItem, Review
 from .serializers import ProductSerializer, CollectionSerializer, ReviewSerializer
 
 
 # We should inherit from "ReadOnlyModelViewSet" if no update and delete method is allowed
 class ProductViewSet(ModelViewSet):
-    def get_queryset(self):
-        queryset = Product.objects.all()
-        collection_id = self.request.query_params.get('collection_id')
-        if collection_id is not None:
-            queryset = queryset.filter(collection_id=collection_id)
-        return queryset
+    queryset = Product.objects.all()
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = ProductFilter
 
     serializer_class = ProductSerializer
 
