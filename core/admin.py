9316a1a85ab576921ecbbb9c1c2c55a5b33915d8
Milad Valizadeh <miladvalizadeh2003@gmail.com>
2024-11-02 08:08:16 +0330

    Extending the User model

--- core/admin.py
+++ core/admin.py
@@ -1,8 +1,24 @@
-from store.models import Product
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.contenttypes.admin import GenericTabularInline
 from store.admin import ProductAdmin
+from store.models import Product
 from tags.models import TaggedItem
+from .models import User
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    # Copied this from "BaseUserAdmin" and only added "email", "first_name" and "last_name"
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("username", "usable_password", "password1", "password2", "email", "first_name", "last_name"),
+            },
+        ),
+    )
 
 
 class TagInline(GenericTabularInline):
--- core/models.py
+++ core/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+# Always create this class in the beginning of a project
+# Even if there is no need for that, use the 'pass' keyword in the body of the class
+# Otherwise you can't migrate it later and get the following error:
+# Migration admin.0001_initial is applied before its dependency core.0001_initial on database 'default'.
+# There is no solution for this and you have to drop the database and create it again
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
