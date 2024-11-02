4effb105c38c3337f5e6eb90ea79b1d6cad90157
Milad Valizadeh <miladvalizadeh2003@gmail.com>
2024-11-02 12:17:54 +0330

    Creating user profiles

--- core/admin.py
+++ core/admin.py
@@ -19,6 +19,7 @@ class UserAdmin(BaseUserAdmin):
             },
         ),
     )
+    search_fields = ['username__istartswith', 'first_name__istartswith', 'last_name__istartswith']
 
 
 class TagInline(GenericTabularInline):
