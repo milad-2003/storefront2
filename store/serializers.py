b6316cda6c6ff3266ab2894e19d251824b8b454e
Milad Valizadeh <miladvalizadeh2003@gmail.com>
2024-11-08 16:12:50 +0330

    Creating order items and deleting the related cart and cart items

--- store/serializers.py
+++ store/serializers.py
@@ -1,4 +1,5 @@
 from decimal import Decimal
+from django.db import transaction
 from rest_framework import serializers
 from store.models import Product, Collection, Review, Cart, CartItem, Customer, Order, OrderItem
 
@@ -130,5 +131,22 @@ class CreateOrderSerializer(serializers.Serializer):
     cart_id = serializers.UUIDField()
 
     def save(self, **kwargs):
-        (customer, created) = Customer.objects.get_or_create(user_id=self.context['user_id'])
-        Order.objects.create(customer=customer)
+        with transaction.atomic():
+            (customer, created) = Customer.objects.get_or_create(user_id=self.context['user_id'])
+            order = Order.objects.create(customer=customer)
+
+            cart_id = self.validated_data['cart_id']
+            cart_items = CartItem.objects \
+                .select_related('product') \
+                .filter(cart_id=cart_id)
+            
+            order_items = [
+                OrderItem(
+                    order=order,
+                    product=item.product,
+                    quantity=item.quantity,
+                    unit_price=item.product.unit_price
+                ) for item in cart_items
+            ]
+            OrderItem.objects.bulk_create(order_items)
+            Cart.objects.filter(pk=cart_id).delete()
